319cd81fb9acee4de000de79e595088f4cb96659
isaac_manubag <isaac_manubag@yahoo.com>
2019-06-06 05:34:28 +0800

    add auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDPUL_kqpWE01g83wk2yK_ZvAXbTuClrU8',
+  authDomain: 'fmc-cebu-feeding.firebaseapp.com',
+};
+firebase.initializeApp(config);
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: '/about',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  ],
+};
 
 function Index() {
-  return <h2>Home</h2>;
+  return (
+    <>
+      <h2>Home</h2>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </>
+  );
 }
 
 function About() {
