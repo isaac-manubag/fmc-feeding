5b25213ef5fef8ef7d405ae612db100357cf7d57
Isaac Manubag <isaac_manubag@yahoo.com>
2019-06-11 10:58:41 +0800

    updates

--- src/components/Dashboard/Kids/kidsForm.js
+++ src/components/Dashboard/Kids/kidsForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -6,8 +6,13 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import firebase from '../../../utils/firebase';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import FileUploader from "react-firebase-file-uploader";
 
 export default function ({ open, onClose }) {
+    const [isUploading, setUploading] = useState(false);
+    const [progress, setProgress] = useState(0);
+    const [avatar, setAvatar] = useState('');
     const fullName = React.createRef();
     const nickName = React.createRef();
     const age = React.createRef();
@@ -24,6 +29,7 @@ export default function ({ open, onClose }) {
                 nickName: nickName.current.value,
                 age: age.current.value,
                 bio: bio.current.value,
+                image: avatar,
             })
             .then(function (docRef) {
                 console.log('Document written with ID: ', docRef.id);
@@ -33,6 +39,39 @@ export default function ({ open, onClose }) {
                 console.error('Error adding document: ', error);
             });
     }
+
+    const handleUploadStart = () => {
+        setUploading(true);
+        setProgress(0);
+    };
+    const handleProgress = progress => setProgress(progress);
+    const handleUploadError = error => {
+        setUploading(false);
+        console.error(error);
+      };
+    
+    const handleUploadSuccess = filename => {
+        setProgress(100);
+        setUploading(false);
+
+        console.table({
+            isUploading,
+            progress,
+            avatar
+        })
+
+        firebase
+          .storage()
+          .ref("images")
+          .child(filename)
+          .getDownloadURL()
+          .then(url => {
+            setAvatar(url);
+
+          });
+      };
+    
+    
 
     return (
         <div>
@@ -73,6 +112,17 @@ export default function ({ open, onClose }) {
                         type="text"
                         fullWidth
                     />
+                    <FileUploader
+                        accept="image/*"
+                        name="avatar"
+                        randomizeFilename
+                        storageRef={firebase.storage().ref("images")}
+                        onUploadStart={handleUploadStart}
+                        onUploadError={handleUploadError}
+                        onUploadSuccess={handleUploadSuccess}
+                        onProgress={handleProgress}
+                    />
+                    {isUploading && <LinearProgress variant="determinate" value={progress} />}
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={onClose} color="secondary">
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,6 +4,7 @@ const config = {
     apiKey: 'AIzaSyDPUL_kqpWE01g83wk2yK_ZvAXbTuClrU8',
     authDomain: 'fmc-cebu-feeding.firebaseapp.com',
     projectId: 'fmc-cebu-feeding',
+    storageBucket: "gs://fmc-cebu-feeding.appspot.com/"
 };
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
