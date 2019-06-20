33bb8310f4287ad690b618d21cf083cbd039e2a6
Isaac Manubag <isaac_manubag@yahoo.com>
2019-06-20 13:23:59 +0800

    updates

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,10 +1,10 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyDPUL_kqpWE01g83wk2yK_ZvAXbTuClrU8",
-  authDomain: "fmc-cebu-feeding.firebaseapp.com",
-  projectId: "fmc-cebu-feeding",
-  storageBucket: "gs://fmc-cebu-feeding.appspot.com/"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
 };
 
 export default (!firebase.apps.length
